0237627342eddb848da7c84e5ffd882dc1a3ed02
vinaypal <vinaypalvirk51@DESKTOP-CP4O2AG>
2024-05-14 21:34:50 -0400

    lailo

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const app = require('./app');
 
-describe('App', () => {
-	  it('should return "Hello, World!"', () => {
-		      assert.strictEqual(app(), 'Hello, World!\n');
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('GET /', () => {
+	  it('should return Hello, World!', (done) => {
+		      chai.request(app)
+		        .get('/')
+		        .end((err, res) => {
+				        expect(res).to.have.status(200);
+				        expect(res.text).to.equal('Hello, World!');
+				        done();
+				      });
 		    });
 });
 
